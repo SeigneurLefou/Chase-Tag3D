b033e1058aeb98a054b4f984a62fb781b395f7f0
SeigneurLefou <loiccha2007@gmail.com>
2026-04-17 19:04:41 +0200

    feat: open window with the GPU feat(print map)!: Render a map with square with the GPU

--- includes/wd_test.hpp
+++ includes/wd_test.hpp
@@ -3,6 +3,23 @@
 
 # include <SDL3/SDL.h>
 # include <SDL3/SDL_main.h>
+# include <SDL3/SDL_timer.h>
 # include <iostream>
+# include <vector>
+
+using namespace std;
+
+typedef struct s_game
+{
+	SDL_Window		*window;
+	SDL_Renderer	*renderer;
+	size_t			map_width;
+	size_t			map_height;
+	size_t			screen_width;
+	size_t			screen_height;
+}			t_game;
+
+bool	init_game(t_game *game);
+void	render_map(t_game game, vector<string> map);
 
 #endif
--- src/window/wd_test.cpp
+++ src/window/wd_test.cpp
@@ -1,44 +1,114 @@
 #include "wd_test.hpp"
 
+bool	init_game(t_game *game)
+{
+	game->screen_width = 800;
+	game->screen_height = 800;
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		SDL_Log("SDL_Init : %s", SDL_GetError());
+		return (EXIT_FAILURE);
+	}
+	game->window = SDL_CreateWindow(
+		"Hello Window", game->screen_width, game->screen_height, 0
+	);
+	if (!game->window)
+	{
+		SDL_Log("SDL_CreateWindow : %s", SDL_GetError());
+		return (EXIT_FAILURE);
+	}
+	game->renderer = SDL_CreateRenderer(game->window, NULL);
+	if (!game->renderer)
+	{
+		SDL_Log("SDL_CreateRenderer : %s", SDL_GetError());
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+void	render_map(t_game game, vector<string> map)
+{
+	int			square_size;
+	SDL_FRect	square;
+
+	square_size = min(game.screen_height / game.map_height, game.screen_width / game.map_width);
+	square.x = 0;
+	square.y = 0;
+	square.w = square_size;
+	square.h = square_size;
+	for (size_t y = 0; y < game.map_height; y++)
+	{
+		cout << "[";
+		square.y = y * square_size;
+		for (size_t x = 0; x < game.map_width; x++)
+		{
+			square.x = x * square_size;
+			switch (map[y][x])
+			{
+				case 1:
+					SDL_SetRenderDrawColor(game.renderer, 200, 200, 200, 255);
+					cout << "1";
+					break;
+				case 2:
+					SDL_SetRenderDrawColor(game.renderer, 200, 16, 16, 255);
+					cout << "2";
+					break;
+				default:
+					SDL_SetRenderDrawColor(game.renderer, 16, 16, 16, 255);
+					cout << "0";
+			}
+			SDL_RenderFillRect(game.renderer, &square);
+		}
+		cout << "]" << endl;
+	}
+}
+
 int main()
 {
 	bool		is_running;
-	SDL_Window*	window;
-	SDL_Renderer*	renderer;
 	SDL_Event	event;
+	t_game		game;
+	vector<string>	map;
+	string			line;
+	char manual_map[7][7] = {
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 1, 1, 0, 1, 1, 0},
+		{0, 1, 0, 0, 0, 1, 0},
+		{0, 0, 0, 2, 0, 0, 0},
+		{0, 1, 0, 0, 0, 1, 0},
+		{0, 1, 1, 0, 1, 1, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+	};
 
-	if (!SDL_Init(SDL_INIT_VIDEO))
-	{
+	if (init_game(&game) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	}
-	window = SDL_CreateWindow(
-		"Hello Window", 800, 300,
-		0
-	);
-	if (!window)
+	game.map_width = 7;
+	game.map_height = 7;
+	for (size_t y = 0; y < game.map_height; y++)
 	{
-		perror("window");
-		return (EXIT_FAILURE);
-	}
-	renderer = SDL_CreateRenderer(window, NULL);
-	if (!renderer)
-	{
-		perror("renderer");
-		return (EXIT_FAILURE);
+		for (size_t x = 0; x < game.map_width; x++)
+		{
+			line.push_back(manual_map[y][x]);
+		}
+		map.push_back(line);
+		line.clear();
 	}
 	is_running = true;
 	while (is_running)
 	{
-		std::cout << "loop" << std::endl;
 		while (SDL_PollEvent(&event))
 		{
-			std::cout << "event loop" << std::endl;
 			if (event.type == SDL_EVENT_QUIT)
 				is_running = false;
 		}
+		SDL_SetRenderDrawColor(game.renderer, 16, 16, 16, 255);
+		SDL_RenderClear(game.renderer);
+		render_map(game, map);
+		SDL_RenderPresent(game.renderer);
+		SDL_Delay(16);
 	}
-		std::cout << "loop" << std::endl;
-	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(game.renderer);
+	SDL_DestroyWindow(game.window);
 	SDL_Quit();
 	return (EXIT_SUCCESS);
 }
